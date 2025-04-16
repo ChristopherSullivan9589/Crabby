406c83e428034505a36b54548fcc316eccfaae24
Joel Sedillo <joelsedillo@Joels-MacBook-Pro.local>
2025-04-15 20:22:32 -0700

    update

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View, StyleSheet } from "react-native";
+import SearchPage from "./SearchPage";
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <SearchPage />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
